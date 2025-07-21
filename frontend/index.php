c7b5516b7b88a35576f5dcb56b10b7d2f906ab93
manhdung1403 <tranmanhdung1403@gmail.com>
2025-07-21 13:59:26 +0700

    Ex04

---
+++ frontend/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Myshop</title>
+<?php
+include_once(__DIR__. '/layout/styles.php');
+?>
+
+
+</head>
+<body>
+
+    <?php include_once(__DIR__ . '/layout/partials/header.php'); ?>
+    <?php include_once(__DIR__ . '/layout/partials/footer.php'); ?>
+    <?php include_once(__DIR__ . '/layout/scripts.php'); ?>
+</body>
+</html>
